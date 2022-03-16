c240ce9e0b9d93c7a5e9b979ea34a89ce224ede0
kiranpatilksp <patilkiransatish881@gmail.com>
2022-03-16 17:15:46 +0530

    first

---
+++ Comparevalues.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class Comparevalues {
+	public static void main(String[] args) {
+	      
+		int y=10;
+		  ++y;
+		System.out.println(y);  //postfix increment operator  //11  11 
+		
+		 
+		 
+ 		
+
+	}
+
+}
